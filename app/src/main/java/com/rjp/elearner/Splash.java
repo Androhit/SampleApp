c32a51a9f0983d0dacf01010edb3bbbd04bcc279
Cloud Next Vision <support@cloudnxtvision.com>
2017-07-29 17:50:12 +0530

    Initial commit

---
+++ app/src/main/java/com/rjp/elearner/Splash.java
@@ -0,0 +1,18 @@
+package com.rjp.elearner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        startActivity(new Intent(this,Login.class));
+    }
+}
